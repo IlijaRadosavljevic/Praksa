61f77d95c2de0b2df1e7851769a2bb5b081b6e9f
IlijaRadosavljevic <ilija.radosavljevic@dev.telnet.rs>
2024-03-06 15:26:28 +0100

    All comments are written acording to guidelines2

--- app/main.py
+++ app/main.py
@@ -3,63 +3,66 @@ from fastapi.params import Body
 from pydantic import BaseModel
 from typing import Optional
 from random import randrange
-app = FastAPI()                         # get metoda
+app = FastAPI()                         
+
                     
 my_posts = [{"title": "title of post 3", "content" : "Content of post 3", "id": 3},
             {"title": "title of post 4", "content" : "Content of post   4", "id": 4}]
 
-
+# Funkcija za pretragu posta sa unetim id-om
 def find_post(id):
     for p in my_posts:
         if p["id"] == id:
             return p
-
+# Funkcija za pretragu indexa posta koji ima uneti id
 def find_index_post(id):
     for i, p in enumerate(my_posts):
         if p['id'] == id:
             return i
 
+# Pravljenje objekta klase Post
 class Post(BaseModel):
     title: str
     content: str
     published: bool = True
     rating: Optional[int] = None
                     
-                                        # uvicorn main:app --reload Pravi live server i monitoruje promene u kodu
-@app.get("/")                           # Decorator
+# Pocetna poruka
+@app.get("/")                           
 async def root():
-     
-    return {"message": "Welcome to my Python"}  # Funkcija
+    return {"message": "Welcome to my Python"}
 
+# Prikaz svih postova pomocu get metode
 @app.get("/posts")
 async def get_posts():
-                                            # Svi postovi
+                                            
     return {"data": my_posts}
 
-                                # Post metoda, pravi se variable payload koji je dictionary json-a iz body-ja postmana.
-                                # Mozemo ga direktno printovati ili postovati na ./createposts
-                                # Title str, content str
+# Post metoda, pravi se variable payload koji je dictionary json-a iz body-ja postmana.
+# Mozemo ga direktno printovati ili postovati na ./createposts
 @app.post("/posts",  status_code = status.HTTP_201_CREATED)                              
 def create_post(post: Post):
-    post_dict = post.dict()                     # Pravljenje postova
+    post_dict = post.dict()                                         # Pravljenje postova
     post_dict['id'] = randrange(0, 1000000)
     my_posts.append(post_dict)
     return {"data": post_dict} 
 
-@app.get("/posts/latest")                       # Uzimanje poslednjeg dodatog posta, mora se staviti iznad pretrazivanja po id jer za vrednost 
-                                                #{id} uzima latest koji nije integer
+# Uzimanje poslednjeg dodatog posta, mora se staviti iznad pretrazivanja po id jer za vrednost 
+# {id} uzima latest koji nije integer
+@app.get("/posts/latest")                       
 def get_latest_post():
     post = my_posts[len(my_posts)-1]    
     return {"detail" : post}
 
+# Pretrazivanje postova po id
 @app.get("/posts/{id}")         
-def get_post(id: int, response: Response):                          # Pretrazivanje postova po id
+def get_post(id: int, response: Response):                          
     post = find_post(id)
     if not post: 
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f" Post with id {id} was not found")
     return {"post_detail": post} 
 
-
+# Brisanje posta koji ima uneti id
 @app.delete("/posts/{id}", status_code= status.HTTP_204_NO_CONTENT)
 def delete_post(id: int,):
     index = find_index_post(id)
@@ -68,6 +71,7 @@ def delete_post(id: int,):
     my_posts.pop(index)
     return Response(status_code= status.HTTP_204_NO_CONTENT)
 
+# Azuriranje posta koji ima uneti id
 @app.put("/posts/{id}")
 def update_post(id: int, post: Post):
     print(post)
