cafebdba191319020ed289b34ebe7f75947ed5b1
IlijaRadosavljevic <ilija.radosavljevic@dev.telnet.rs>
2024-03-13 14:16:54 +0100

    Fix minor bugs

--- app/oauth2.py
+++ app/oauth2.py
@@ -11,7 +11,7 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 
 SECRET_KEY = settings.secret_key
 ALGORITHM = settings.algorithm
-ACCESS_TOKEN_EXPIRE_MINUTES = settings.access_token_expire_minutes
+ACCESS_TOKEN_EXPIRE_MINUTES = settings.access_token_expire_minutes  
 
 
 # Metoda za kreiranje tokena koji vazi 60min
